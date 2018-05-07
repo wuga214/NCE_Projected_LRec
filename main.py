98ea910640c8c3ab2e0a64b8dca2b029d6069a19
Wuga <wuga214@gmail.com>
2018-05-07 18:26:23 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import numpy as np
+from scipy.sparse import load_npz, save_npz
+from models.lrec import embedded_lirec_items, embedded_lirec_users
+from utils.progress import WorkSplitter, inhour
+import argparse
+import time
+
+
+PATH = '/media/wuga/Storage/python_project/lrec/data/'
+TRAIN_NPY = PATH+'R_train.npz'
+VALID_NPY = PATH+'R_valid.npz'
+ROWS_NPY = PATH+'validRows.npy'
+RANK = 200
+
+
+def check_positive(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+         raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
+    return ivalue
+
+
+def main(args):
+    progress = WorkSplitter()
+    progress.section("Loading Data")
+    start_time = time.time()
+    R_train = load_npz(TRAIN_NPY).tocsr()
+    R_valid = load_npz(VALID_NPY).tocsr()
+    valid_rows = np.load(ROWS_NPY)
+    print "Elapsed: {0}".format(inhour(time.time() - start_time))
+
+    print("Train U-I Dimensions: {0}".format(R_train.shape))
+    print("Valid U-I Dimensions: {0}".format(R_valid.shape))
+
+    if args.item == True:
+        progress.section("Item-Item Similarity")
+        RQ, Y = embedded_lirec_items(R_train, embeded_matrix=np.empty((0)), iteration=args.iter, lam=80, rank=RANK)
+        print type(RQ)
+        print RQ.shape
+        print type(Y)
+        print Y.shape
+        progress.section("Save U-V Matrix")
+        start_time = time.time()
+        save_npz(PATH + 'U_{0}.npy'.format(RANK), RQ)
+        save_npz(PATH + 'V_{0}.npy'.format(RANK), Y)
+        print "Elapsed: {0}".format(inhour(time.time() - start_time))
+    else:
+        progress.section("User-User Similarity")
+        PtR, Y = embedded_lirec_users(R_train, embeded_matrix=np.empty((0)), iteration=args.iter, lam=80, rank=RANK)
+        print type(PtR)
+        print PtR.shape
+        print type(Y)
+        print Y.shape
+        progress.section("Save U-V Matrix")
+        start_time = time.time()
+        save_npz(PATH + 'U_{0}.npy'.format(RANK), Y)
+        save_npz(PATH + 'V_{0}.npy'.format(RANK), PtR)
+        print "Elapsed: {0}".format(inhour(time.time() - start_time))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Projected LRec")
+
+    parser.add_argument('--disable-item-item', dest='item', action='store_false')
+    parser.add_argument('-i', dest='iter', type=check_positive)
+    args = parser.parse_args()
+
+    main(args)
---
+++ models/lrec.py
@@ -0,0 +1,71 @@
+import numpy as np
+import scipy.sparse as sparse
+from scipy.sparse import vstack, hstack
+from scipy.sparse.linalg import inv
+from sklearn.utils.extmath import randomized_svd
+from utils.progress import WorkSplitter, inhour
+import time
+
+
+def embedded_lirec_items(matrix_train, embeded_matrix=np.empty((0)), iteration=4, lam=80, rank=200):
+    """
+    Function used to achieve generalized projected lrec w/o item-attribute embedding
+    :param matrix_train: user-item matrix with shape m*n
+    :param embeded_matrix: item-attribute matrix with length n (each row represents one item)
+    :param lam: parameter of penalty
+    :param k_factor: ratio of the latent dimension/number of items
+    :return: prediction in sparse matrix
+    """
+    progress = WorkSplitter()
+    matrix_input = matrix_train
+    if embeded_matrix.shape[0] > 0:
+        matrix_input = vstack((matrix_input, embeded_matrix.T))
+
+    progress.section("Randomized SVD with {0} iterations".format(iteration))
+    start_time = time.time()
+    P, sigma, Qt = randomized_svd(matrix_input,
+                                  n_components=rank,
+                                  n_iter=iteration,
+                                  random_state=None)
+    RQ = matrix_input * sparse.csr_matrix(Qt).T
+    print "Elapsed: {0}".format(inhour(time.time() - start_time))
+
+    progress.section("Closed-Form Linear Optimization")
+    start_time = time.time()
+    pre_inv = RQ.T.dot(RQ) + lam * sparse.identity(rank)
+    inverse = sparse.csr_matrix(inv(pre_inv))
+    Y = inverse.dot(RQ.T).dot(matrix_input)
+    print "Elapsed: {0}".format(inhour(time.time() - start_time))
+    return RQ, Y
+
+
+def embedded_lirec_users(matrix_train, embeded_matrix=np.empty((0)), iteration=4, lam=80, rank=200):
+    """
+    Function used to achieve generalized projected lrec w/o item-attribute embedding
+    :param matrix_train: user-item matrix with shape m*n
+    :param embeded_matrix: user-attribute matrix with length m (each row represents one user)
+    :param lam: parameter of penalty
+    :param k_factor: ratio of the latent dimension/number of items
+    :return: prediction in sparse matrix
+    """
+    progress = WorkSplitter()
+    matrix_nonzero = matrix_train.nonzero()
+    matrix_input = matrix_train
+    if embeded_matrix.shape[0] > 0:
+        matrix_input = hstack((matrix_input, embeded_matrix))
+    progress.section("Randomized SVD with {0} iterations".format(iteration))
+    start_time = time.time()
+    P, sigma, Qt = randomized_svd(matrix_input,
+                                  n_components=rank,
+                                  n_iter=iteration,
+                                  random_state=None)
+    print "Elapsed: {0}".format(inhour(time.time() - start_time))
+    PtR = sparse.csr_matrix(P).T.dot(matrix_input)
+
+    progress.section("Closed-Form Linear Optimization")
+    start_time = time.time()
+    pre_inv = PtR.dot(PtR.T) + lam * sparse.identity(rank)
+    inverse = sparse.csr_matrix(inv(pre_inv))
+    Y = matrix_input.dot(PtR.T).dot(inverse)
+    print "Elapsed: {0}".format(inhour(time.time() - start_time))
+    return PtR, Y
