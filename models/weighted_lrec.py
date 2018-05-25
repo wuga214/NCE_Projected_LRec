fbd4c383aec1f9f2c58ca111bb5a15a4989ff892
Wuga <wuga214@gmail.com>
2018-05-25 12:51:01 -0400

    Nice result

--- models/weighted_lrec.py
+++ models/weighted_lrec.py
@@ -57,7 +57,7 @@ def weighted_lrec_items(matrix_train,
 
     progress.section("Randomized SVD")
     start_time = time.time()
-    P, sigma, Qt = randomized_svd(matrix_input, n_components=rank, n_iter=iteration, random_state=None)
+    P, sigma, Qt = randomized_svd(matrix_input, n_components=rank, n_iter=iteration, random_state=1)
     print "Elapsed: {0}".format(inhour(time.time() - start_time))
 
     start_time = time.time()
