6a55d4061330c0a1245f4756df7f4b7b5620ac9c
wuga <wuga214@gmail.com>
2018-05-26 23:54:16 -0400

    ALS Done

--- models/weighted_lrec.py
+++ models/weighted_lrec.py
@@ -68,10 +68,10 @@ def weighted_lrec_items(matrix_train,
         # Exact
         matrix_B = cp.array(RQ)
         matrix_BT = matrix_B.T
-        matrix_A = matrix_BT.dot(matrix_B) - cp.array((lam * sparse.identity(rank)).toarray())
+        matrix_A = matrix_BT.dot(matrix_B) + cp.array((lam * sparse.identity(rank)).toarray())
 
         # Approx
-        # matrix_A = cp.array(sparse.diags(sigma * sigma - lam).todense())
+        # matrix_A = cp.array(sparse.diags(sigma * sigma + lam).todense())
         # matrix_B = cp.array(P*sigma)
         # matrix_BT = cp.array(matrix_B.T)
         print "Elapsed: {0}".format(inhour(time.time() - start_time))
@@ -99,7 +99,7 @@ def weighted_lrec_items(matrix_train,
         # Exact
         matrix_B = RQ
         matrix_BT = RQ.T
-        matrix_A = matrix_BT.dot(matrix_B) - (lam * sparse.identity(rank)).toarray()
+        matrix_A = matrix_BT.dot(matrix_B) + (lam * sparse.identity(rank)).toarray()
 
         # Approx
         # matrix_B = P * sigma
