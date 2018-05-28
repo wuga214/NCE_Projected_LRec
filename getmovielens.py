e35c00200e928b29d183152a08af05e201a1aaeb
Wuga <wuga214@gmail.com>
2018-05-28 13:39:42 -0400

    Modify split function

--- getmovielens.py
+++ getmovielens.py
@@ -1,30 +1,32 @@
 from utils.io import save_numpy, load_pandas
-from utils.argument import shape, check_float_positive
-from providers.split import split
+from utils.argument import shape, ratio
+from providers.split import time_ordered_split
 import argparse
-from utils.progress import WorkSplitter, inhour
+from utils.progress import WorkSplitter
 
 
 def main(args):
     progress = WorkSplitter()
 
     progress.section("Load Raw Data")
-    matrix = load_pandas(path=args.path, name=args.name, shape=args.shape)
+    rating_matrix = load_pandas(path=args.path, name=args.name, shape=args.shape)
+    timestamp_matrix = load_pandas(path=args.path, value_name='timestamp', name=args.name, shape=args.shape)
     progress.section("Split CSR Matrices")
-    rtrain, rvalid = split(matrix=matrix, ratio=args.ratio, implicit=args.implicit, random=args.random)
+    rtrain, rvalid, rtest = time_ordered_split(rating_matrix=rating_matrix, timestamp_matrix=timestamp_matrix,
+                                               ratio=args.ratio, implicit=args.implicit)
     progress.section("Save NPZ")
     save_numpy(rtrain, args.path, "Rtrain")
     save_numpy(rvalid, args.path, "Rvalid")
+    save_numpy(rtest, args.path, "Rvtest")
 
 if __name__ == "__main__":
     # Commandline arguments
     parser = argparse.ArgumentParser(description="Data Preprocess")
     parser.add_argument('--implicit', dest='implicit', action='store_true')
-    parser.add_argument('--random-split', dest='random', action='store_true')
-    parser.add_argument('-r', dest='ratio', type=check_float_positive, default=0.3)
+    parser.add_argument('-r', dest='ratio', type=ratio, default='0.5,0.2,0.3')
     parser.add_argument('-d', dest='path',
-                        default="/media/wuga/Storage/python_project/wlrec/IMPLEMENTATION_Projected_LRec/datax/")
-    parser.add_argument('-n', dest='name', default='ml-20m/ratings.csv')
+                        default="/media/wuga/Storage/python_project/wlrec_update/IMPLEMENTATION_Projected_LRec/datax/")
+    parser.add_argument('-n', dest='name', default='ml-1m/ratings.csv')
     parser.add_argument('--shape', help="CSR Shape", dest="shape", type=shape, nargs=2)
     args = parser.parse_args()
 
--- utils/argument.py
+++ utils/argument.py
@@ -15,9 +15,19 @@ def check_float_positive(value):
          raise argparse.ArgumentTypeError("%s is an invalid positive float value" % value)
     return ivalue
 
+
 def shape(s):
     try:
         num = int(s)
         return num
     except:
         raise argparse.ArgumentTypeError("Sparse matrix shape must be integer")
+
+
+def ratio(s):
+    try:
+        train, valid, test = map(float, s.split(','))
+        assert train + valid + test == 1
+        return train, valid, test
+    except:
+        raise argparse.ArgumentTypeError("The sum of ratios must equals to one")
