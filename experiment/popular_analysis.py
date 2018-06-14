3981c752ea1f77240d5b06ad6a293f7b0ad87d65
Wuga <wuga214@gmail.com>
2018-06-14 17:43:20 -0400

    Half done work

--- experiment/popular_analysis.py
+++ experiment/popular_analysis.py
@@ -11,10 +11,18 @@ def get_popular_predictions(predict_matrix, shape):
     for i, user in enumerate(predict_matrix):
         predict = predict + sparse.csr_matrix((np.ones(k), (np.full(k, i), user)), shape)
 
-    return np.sort(np.sum(predict, axis = 0))
+    item_popularity = np.sum(predict, axis=0)
+
+    item_popularity_rated = np.array(item_popularity/np.sum(item_popularity)).flatten()
+
+    order = np.argsort(item_popularity_rated)
+    return order, item_popularity_rated
 
 
+def popular_overlapping(train_matrix, predict_matrix, k=20):
+    train_popularity = np.sum(train_matrix, axis=0)
+    train_popularity_rated = np.array(train_popularity/np.sum(train_popularity)).flatten()
+    order_train = np.argsort(train_popularity_rated)
+    order_predict, predict_popularity_rated = get_popular_predictions(predict_matrix, train_matrix.shape)
+    import ipdb; ipdb.set_trace()
 
-
-# def popular_overlapping(train_matrix, predict_matrix, names):
-#
--- run.py
+++ run.py
@@ -1,14 +1,47 @@
 import argparse
 from utils.io import load_numpy
+import numpy as np
 from experiment.latent_analysis import latent_analysis
+from experiment.popular_analysis import popular_overlapping
 from utils.argument import shape
+from utils.argument import check_float_positive, check_int_positive, shape
+
+from models.lrec import embedded_lrec_items
+from models.weighted_lrec import weighted_lrec_items
+from models.pure_svd import pure_svd, eigen_boosted_pure_svd
+from models.als import als
+from models.pmi_lrec import pmi_lrec_items
+from models.weighted_pmi_lrec import weighted_pmi_lrec_items
+from models.chainitemitem import chain_item_item
+from models.predictor import predict
+
+
+models = {
+    "PLRec": embedded_lrec_items,
+    "WPLRec": weighted_lrec_items,
+    "PmiPLRec": pmi_lrec_items,
+    "PmiWPLRec": weighted_pmi_lrec_items,
+    "PureSVD": pure_svd,
+    "EBPureSVD": eigen_boosted_pure_svd,
+    "ALS": als,
+    "CII": chain_item_item,
+}
 
 
 
 def main(args):
     R_train = load_numpy(path=args.path, name=args.train)
-    latent_analysis(R_train)
+    #latent_analysis(R_train)
 
+    RQ, Yt = models['PLRec'](R_train, embeded_matrix=np.empty((0)),
+                             iteration=args.iter, rank=args.rank,
+                             lam=args.lamb, alpha=args.alpha, seed=args.seed, root=args.root)
+
+    Y = Yt.T
+
+    prediction = predict(matrix_U=RQ, matrix_V=Y, topK=10, matrix_Train=R_train, gpu=True)
+
+    popular_overlapping(R_train, prediction)
 
 if __name__ == "__main__":
     # Commandline arguments
@@ -17,6 +50,12 @@ if __name__ == "__main__":
     parser.add_argument('-t', dest='train', default='Rtrain.npz')
     parser.add_argument('-v', dest='valid', default='Rvalid.npz')
     parser.add_argument('--shape', help="CSR Shape", dest="shape", type=shape, nargs=2)
+    parser.add_argument('-i', dest='iter', type=check_int_positive, default=4)
+    parser.add_argument('-a', dest='alpha', type=check_float_positive, default=1.0)
+    parser.add_argument('-l', dest='lamb', type=check_float_positive, default=1.0)
+    parser.add_argument('-r', dest='rank', type=check_int_positive, default=100)
+    parser.add_argument('-f', dest='root', type=check_float_positive, default=1)
+    parser.add_argument('-s', dest='seed', type=check_int_positive, default=1)
     args = parser.parse_args()
 
     main(args)
