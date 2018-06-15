6ba7ec23787264b299e3b7351acfcdfaf7e09668
Wuga <wuga214@gmail.com>
2018-06-15 16:02:36 -0400

    Minors

--- experiment/tuning.py
+++ experiment/tuning.py
@@ -55,7 +55,7 @@ def hyper_parameter_tuning(train, validation, params):
                     result_dict = {'model': algorithm, 'rank': rank, 'root': root, 'alpha': alpha}
 
                     for name in result.keys():
-                        result_dict[name] = [round(result[name][0], 5), round(result[name][1], 5)]
+                        result_dict[name] = [round(result[name][0], 4), round(result[name][1], 4)]
 
                     df = df.append(result_dict, ignore_index=True)
     return df
