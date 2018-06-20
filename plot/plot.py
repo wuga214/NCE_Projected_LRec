893af3c7a06abfb0e7ccaf145ea3f83565d0e1e2
Wuga <wuga214@gmail.com>
2018-06-20 15:34:10 -0400

    Sensitivity hyper-parameter settings

--- plot/plot.py
+++ plot/plot.py
@@ -3,15 +3,9 @@ import seaborn as sns
 
 
 def curve_weighting(lrec_result, wlrec_results, weights, metric, name):
-    plt.figure(figsize=(4, 3))
+    plt.figure(figsize=(4, 2.5))
     plt.hlines(lrec_result[metric][0], weights[0], weights[-1],
                color=sns.xkcd_rgb["pale red"], linestyle='--', label='Projected LRec')
-    #
-    # plt.hlines(lrec_result[metric][0]+lrec_result[metric][1], weights[0], weights[-1],
-    #            color=sns.xkcd_rgb["pale red"], linestyle='--')
-    #
-    # plt.hlines(lrec_result[metric][0]-lrec_result[metric][1], weights[0], weights[-1],
-    #            color=sns.xkcd_rgb["pale red"], linestyle='--')
 
     scores = []
     errs = []
@@ -23,6 +17,26 @@ def curve_weighting(lrec_result, wlrec_results, weights, metric, name):
     plt.legend(loc=4)
     #plt.xscale('log')
     plt.xlabel('Alpha')
+    plt.ylabel(metric)
+    plt.tight_layout()
+    plt.savefig('figures/{0}.pdf'.format(name), format='pdf')
+    plt.show()
+
+
+def curve_sensitivity(lrec_result, wlrec_results, weights, metric, name):
+    plt.figure(figsize=(4, 2.5))
+    plt.hlines(lrec_result[metric][0], weights[0], weights[-1],
+               color=sns.xkcd_rgb["pale red"], linestyle='--', label='Default PMI')
+
+    scores = []
+    errs = []
+    for i in weights:
+        scores.append(wlrec_results[i][metric][0])
+        errs.append(wlrec_results[i][metric][1])
+
+    plt.plot(weights, scores, sns.xkcd_rgb["denim blue"], label='Modified PMI')
+    plt.legend(loc=4)
+    plt.xlabel('Beta')
     plt.ylabel(metric)
     plt.tight_layout()
     plt.savefig('figures/{0}.pdf'.format(name), format='pdf')
