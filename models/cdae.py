95ba6207d00e19f50ec8711c2476ac1338cd1e1d
wuga <wuga214@gmail.com>
2018-11-07 14:22:26 -0500

    Fix CDAE

--- models/cdae.py
+++ models/cdae.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.python.ops import math_ops
 import numpy as np
 from tqdm import tqdm
 from utils.progress import WorkSplitter, inhour
@@ -34,34 +35,36 @@ class CDAE(object):
         with tf.variable_scope('encode'):
 
             # Normal Encoder Part
-            encode_weights = tf.Variable(tf.random_normal([self.input_dim, self.embed_dim],
-                                                            stddev=1 / (self.embed_dim ** 0.5), dtype=tf.float32),
+            encode_weights = tf.Variable(tf.truncated_normal([self.input_dim, self.embed_dim], stddev=1 / 500.0),
                                          name="Weights")
             encode_bias = tf.Variable(tf.constant(0., shape=[self.embed_dim]), name="Bias")
 
             # User Representation
-            user_embeddings = tf.Variable(tf.random_normal([self.user_dim, self.embed_dim],
-                                                            stddev=1 / (self.embed_dim ** 0.5), dtype=tf.float32),
+            user_embeddings = tf.Variable(tf.truncated_normal([self.user_dim, self.embed_dim], stddev=1 / 500.0),
                                           name="UserEmbeddings")
 
-            users = tf.nn.embedding_lookup(user_embeddings, self.user_id, name="users")
+            users = tf.stop_gradient(tf.nn.embedding_lookup(user_embeddings, self.user_id, name="users"))
 
             # Corrupt Inputs
             corrupped_inputs = tf.nn.dropout(self.inputs, 1-self.corruption)
 
             self.encoded = tf.nn.relu(tf.matmul(corrupped_inputs, encode_weights) + users + encode_bias)
 
+
         with tf.variable_scope('decode'):
-            self.decode_weights = tf.Variable(tf.random_normal([self.embed_dim, self.output_dim],
-                                                            stddev=1 / (self.embed_dim ** 0.5), dtype=tf.float32),
-                                         name="Weights")
+            self.decode_weights = tf.Variable(tf.truncated_normal([self.embed_dim, self.output_dim], stddev=1 / 500.0),
+                                              name="Weights")
             self.decode_bias = tf.Variable(tf.constant(0., shape=[self.output_dim]), name="Bias")
             prediction = tf.matmul(self.encoded, self.decode_weights) + self.decode_bias
 
         with tf.variable_scope('loss'):
-            l2_loss = tf.nn.l2_loss(encode_weights) + tf.nn.l2_loss(self.decode_weights)
+            l2_loss = tf.nn.l2_loss(encode_weights) + tf.nn.l2_loss(self.decode_weights) + tf.nn.l2_loss(users)
             sigmoid_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=self.inputs, logits=prediction)
-            self.loss = tf.reduce_mean(sigmoid_loss) + self.lamb*tf.reduce_mean(l2_loss)
+
+            mask = tf.stop_gradient(math_ops.floor(corrupped_inputs))
+            weighted_sigmoid_loss = mask*(1-self.corruption)*sigmoid_loss + (1-mask)*self.corruption*sigmoid_loss
+
+            self.loss = tf.reduce_mean(weighted_sigmoid_loss) + self.lamb * tf.reduce_mean(l2_loss)
 
         with tf.variable_scope('optimizer'):
             self.optimizer = tf.train.AdamOptimizer().minimize(self.loss)
@@ -99,7 +102,7 @@ class CDAE(object):
         for step in range(len(batches)):
             feed_dict = {self.inputs: batches[step][0].todense(),
                          self.user_id: batches[step][1],
-                         self.corruption: 1.}
+                         self.corruption: 0.}
             encoded = self.sess.run(self.encoded, feed_dict=feed_dict)
             RQ.append(encoded)
 
@@ -112,7 +115,7 @@ class CDAE(object):
         return self.sess.run(self.decode_bias)
 
 
-def cdae(matrix_train, embeded_matrix=np.empty((0)), iteration=100, lam=80, rank=200, corrpution=0.8, seed=1, **unused):
+def cdae(matrix_train, embeded_matrix=np.empty((0)), iteration=100, lam=80, rank=200, corruption=0.8, seed=1, **unused):
     progress = WorkSplitter()
     matrix_input = matrix_train
     if embeded_matrix.shape[0] > 0:
@@ -121,7 +124,7 @@ def cdae(matrix_train, embeded_matrix=np.empty((0)), iteration=100, lam=80, rank
     m, n = matrix_input.shape
     model = CDAE(m, n, rank, 100, lamb=lam)
 
-    model.train_model(matrix_input, 0.8, iteration)
+    model.train_model(matrix_input, corruption, iteration)
 
     RQ = model.get_RQ(matrix_input)
     Y = model.get_Y()
